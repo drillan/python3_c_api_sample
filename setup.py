3242690de7b4134deb01a999837779a055d32439
driller <eleshis@gmail.com>
2016-12-11 13:05:23 +0900

    setup.pyのif __name__ == '__main__':を削除

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, Extension
 
-if __name__ == '__main__':
-    c = Extension('c', sources=['prim.c'])
-    setup(name="api_sample", version="0.0.0",
-          description="Python3 API Sample",
-          packages=['api_sample'], ext_modules=[c])
+c = Extension('c', sources=['prim.c'])
+setup(name="api_sample", version="0.0.0",
+      description="Python3 API Sample",
+      packages=['api_sample'], ext_modules=[c])
